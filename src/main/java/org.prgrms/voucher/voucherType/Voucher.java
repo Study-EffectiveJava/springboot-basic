4424e7550de314e6900ae2563b65f7293e0b8fc7
yewon9609 <dodnjs1241@naver.com>
2022-11-19 00:30:29 +0900

    style: 파일경로 변경

---
+++ src/main/java/org.prgrms/voucher/voucherType/Voucher.java
@@ -0,0 +1,17 @@
+package org.prgrms.voucher.voucherType;
+
+import java.util.UUID;
+import org.prgrms.voucher.discountType.Amount;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface Voucher {
+  long discount(long beforeDiscount);
+
+  UUID getVoucherId();
+
+  Amount getVoucherAmount();
+
+  VoucherType getVoucherType();
+
+}
